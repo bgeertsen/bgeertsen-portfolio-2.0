3c5e8bff1cb5746a8cf91d401e32a809eaf17693
bg <62928419+bgeertsen@users.noreply.github.com>
2022-09-24 13:41:37 -0700

    updated resume component

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,7 +1,36 @@
 import React from "react";
 
 function Resume() {
-  return <div>hello</div>;
+  return (
+    <div className="resume">
+      <a
+        className="download"
+        href={require(`../../Assets/docs/Resume2022.pdf`)}
+        download
+      >
+        Download
+      </a>
+      <h1>Resume</h1>
+      <h2>Front-end Proficiencies</h2>
+      <uL>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>jQuery</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+        <li>Responsive design</li>
+      </uL>
+      <h2>Back-end Proficiencies</h2>
+      <ul>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MySQL, Sequelize</li>
+        <li>MongoDB, Mongoose</li>
+        <li>GraphQL, Apollo</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Resume;
